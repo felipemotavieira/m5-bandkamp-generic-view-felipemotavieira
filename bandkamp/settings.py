1f19b31651af3001befadc5f27614d229c9c3be6
felipemotavieira <felipemtvieira@gmail.com>
2022-12-16 17:03:50 -0300

    arrumando procfile4

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -95,12 +95,10 @@ WSGI_APPLICATION = "bandkamp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv('POSTGRES_DB'),
         "USER": os.getenv('POSTGRES_USER'),
         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
-        "HOST": "127.0.0.1",
-        "PORT": 5432,
     }
 }
 
